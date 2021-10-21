63b5f75a8c0fae661ab59940bf10b35202a3d9a0
Alex - RoX_KD <Alex.t.h.93@hotmail.com>
2021-10-21 15:18:01 +0200

    add gameplan

--- Mixed Messages/script.js
+++ Mixed Messages/script.js
@@ -3,7 +3,14 @@
 // made by RoX_KD & BAM 
 // ---------------------------------------------------------------------------------
 
-// Objectives - Visualize end result
+// Objectives - Visualize end result... ## 1 
 // 1. Choose a subject (4 subjects to create messages from)
 // 2. Create a random messages based on 20 possibillities from 4 categories (In total 80 messages must be created)
 // 3. Decide functionallity (arrays, built methods or functions, objects or prewritten possibilities with numeric value based on subject.)
+
+
+// Objectives - Visualize end result... ## 2
+// 1. Choose a subject
+// 2. Create random words and store in an array or numeric possibilites
+// 3. Create a function/method which through an algorithm, creates a choesive message based on rules of words (verbs, nouns, adjectives, adverbs, prepositions, pronouns,
+// interjections and suffixes). 
