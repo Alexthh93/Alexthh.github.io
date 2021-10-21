420402e116af41daff4d002a782d2af0f491e09e
Alex - RoX_KD <Alex.t.h.93@hotmail.com>
2021-10-21 15:07:06 +0200

    start project

---
+++ Mixed Messages/script.js
@@ -0,0 +1,9 @@
+// Create a message generator - 
+
+// made by RoX_KD & BAM 
+// ---------------------------------------------------------------------------------
+
+// Objectives - Visualize end result
+// 1. Choose a subject (4 subjects to create messages from)
+// 2. Create a random messages based on 20 possibillities from 4 categories (In total 80 messages must be created)
+// 3. Decide functionallity (arrays, built methods or functions, objects or prewritten possibilities with numeric value based on subject.)
